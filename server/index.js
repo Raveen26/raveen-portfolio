3bc41f68579282f8a4094edc09d11e4bc40ddf49
Raveen26 <raveensangaran10@gmail.com>
2026-06-08 23:23:49 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
